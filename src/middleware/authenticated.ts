632bcee24656373d2f7f1c4c3cbf777d93eda4cc
Alex Benedetto <alex061994@gmail.com>
2020-03-30 10:09:01 -0700

    added structured unauthorized exception

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -6,16 +6,16 @@ import { AuthRequest } from '../types';
 
 export const authenticate = async (req: AuthRequest, res: Response, next: NextFunction) => {
   const idToken = req.headers.authorization;
-  if (!idToken) return res.status(401).send('unauthorized');
+  if (!idToken) return res.status(401).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
   try {
     const decodedToken = await Firebase.client.auth().verifyIdToken(idToken);
-    if (!decodedToken) return res.status(401).send('unauthorized');
+    if (!decodedToken) return res.status(401).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
     req.user = { id: decodedToken.user_id, email: decodedToken.email };
     return next();
   } catch (e) {
     const secret = `Bearer ${Secrets.bearerToken}`;
     const authorizationBuffer = idToken.length !== secret.length ? Buffer.alloc(secret.length, 0) : Buffer.from(idToken, 'utf-8');
     if (crypto.timingSafeEqual(authorizationBuffer, Buffer.from(secret, 'utf-8'))) return next();
-    return res.status(401).send('unauthorized');
+    return res.status(401).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
   }
 };
