b94323180289f81198674dcf8dd165ae6a102261
noahgribbin <noahagribbin@gmail.com>
2020-04-02 11:28:31 -0700

    fix migrations

--- src/migrations/1585851953206-ExtendActions.ts
+++ src/migrations/1585851953206-ExtendActions.ts
@@ -1,18 +1,17 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class ExtendActions1585851953206 implements MigrationInterface {
-    name = 'ExtendActions1585851953206'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "participant" ADD "viewCount" integer NOT NULL DEFAULT 0`, undefined);
-        await queryRunner.query(`ALTER TABLE "participant" ADD "submissionCount" integer NOT NULL DEFAULT 0`, undefined);
-        await queryRunner.query(`ALTER TABLE "campaign" ADD "totalParticipationScore" numeric NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "participant" ADD "viewCount" integer NOT NULL DEFAULT 0`);
+        await queryRunner.query(`ALTER TABLE "participant" ADD "submissionCount" integer NOT NULL DEFAULT 0`);
+        await queryRunner.query(`ALTER TABLE "campaign" ADD "totalParticipationScore" numeric NOT NULL DEFAULT 0`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "campaign" DROP COLUMN "totalParticipationScore"`, undefined);
-        await queryRunner.query(`ALTER TABLE "participant" DROP COLUMN "submissionCount"`, undefined);
-        await queryRunner.query(`ALTER TABLE "participant" DROP COLUMN "viewCount"`, undefined);
+        await queryRunner.query(`ALTER TABLE "campaign" DROP COLUMN "totalParticipationScore"`);
+        await queryRunner.query(`ALTER TABLE "participant" DROP COLUMN "submissionCount"`);
+        await queryRunner.query(`ALTER TABLE "participant" DROP COLUMN "viewCount"`);
     }
 
 }
