a13a3ef66a712be9136e3cf2ec83fae760abbf7a
Alex Benedetto <alex@dragonchain.com>
2020-04-22 16:01:43 -0700

    finished shortener (#68)

---
+++ src/clients/tinyUrl.ts
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+export class TinyUrl {
+  public static async shorten(url: string) {
+    const res = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if (!res.ok) throw new Error(`An error occurred while generating the tiny url: ${res.statusText}`);
+    return res.text();
+  }
+
+  public static async resolve(url: string) {
+    const res = await fetch(url);
+    if (!res.headers.get('location')) throw new Error('Tiny URL not found');
+    return res.headers.get('location');
+  }
+}
