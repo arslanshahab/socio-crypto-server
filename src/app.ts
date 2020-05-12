efbe451ed66919d75a59479e7effc29d50b5d6d5
Alex Benedetto <alex061994@gmail.com>
2020-05-12 16:44:01 -0700

    wip

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import { authenticate } from './middleware/authentication';
 import { requestLogger } from './middleware/logging';
 import { Dragonchain } from './clients/dragonchain';
 import * as Dragonfactor from './controllers/dragonfactor';
+import { ExpressDragonfactor } from '@dragonchain-dev/express-dragonfactor';
 
 const { NODE_ENV = 'development' } = process.env;
 
@@ -58,7 +59,7 @@ export class Application {
       graphiql: NODE_ENV === 'development',
     }));
     this.app.get('/v1/health', (_req: express.Request, res: express.Response) => res.send('I am alive and well, thank you!'));
-    this.app.post('/v1/dragonfactor/login', Dragonfactor.login);
+    this.app.use('/v1/dragonfactor/login', ExpressDragonfactor({ service: 'raiinmaker', acceptedFactors: ['email'] }), Dragonfactor.login);
   }
 
   public async startServer() {
--- src/controllers/dragonfactor.ts
+++ src/controllers/dragonfactor.ts
@@ -1,36 +1,8 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '../util/helpers';
-import { Validator } from '../util/validator';
-import { DragonfactorLoginRequest, GenericFactor } from '../types';
-import {
-  verifyFactor,
-  verifyCrossSigning,
-  verifyLoginRequestSignature,
-} from '../util/crypto';
-
-const validFactors = ['email'];
-const validator = new Validator();
-const TIME_DIFFERENCE = 10000; // 5 seconds in milliseconds
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
-  console.log(req.body);
-  validator.validateDragonfactorLogin(req.body);
-  console.log('validated body');
-  const factorRequest: DragonfactorLoginRequest = req.body;
-  if (!validFactors.includes(factorRequest.factorType)) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  console.log('factor is an accepted type');
-  // check that the timestamp is within acceptable bounds
-  if (new Date(factorRequest.timestamp).getUTCMilliseconds() > new Date().getUTCMilliseconds()) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  console.log('timestamp is not in the future');
-  if (new Date().getUTCMilliseconds() - new Date(factorRequest.timestamp).getUTCMilliseconds() > TIME_DIFFERENCE) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  console.log('timestamp is within valid timestamp');
-  const factor: GenericFactor = JSON.parse(factorRequest.factor);
-  if (!verifyLoginRequestSignature(factorRequest)) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  console.log('login request signature is valid');
-  if (!verifyFactor(factor)) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  console.log('factor is valid');
-  if (!verifyCrossSigning(factorRequest.factorAssociation, factorRequest.signingPublicKey)) return res.status(403).json({ code: 'UNAUTHORIZED', message: 'unauthorized' });
-  // handle either signing in or signing up here
+  console.log((req as any).user);
   return res.status(200).json({ success: true, token: 'banana' });
 });
 
