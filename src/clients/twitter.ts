b6ff235877a49a0b5eb09a19492e38df425bfe34
Alex Benedetto <alex061994@gmail.com>
2020-05-19 10:52:08 -0700

    return only strings for getting tweets

--- src/clients/twitter.ts
+++ src/clients/twitter.ts
@@ -30,16 +30,16 @@ export class TwitterClient {
     return response.id_str;
   }
 
-  public static get = async (credentials: SocialClientCredentials, id: string, cached = true): Promise<Twitter.ResponseData> => {
+  public static get = async (credentials: SocialClientCredentials, id: string, cached = true): Promise<String> => {
     logger.debug(`retrieving tweet with id: ${id}`);
     let cacheKey = `twitter:${id}`;
     if (cached) {
       const cachedResponse = await getRedis().get(cacheKey);
-      if (cachedResponse) return JSON.parse(cachedResponse);
+      if (cachedResponse) return cachedResponse;
     }
     const client = TwitterClient.getClient(credentials);
     const twitterResponse = await client.get('/statuses/show', {id});
     await getRedis().set(cacheKey, JSON.stringify(twitterResponse), 'EX', 900); // cache for 15 minutes
-    return twitterResponse;
+    return JSON.stringify(twitterResponse);
   }
 }
