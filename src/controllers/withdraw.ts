e24c3cc76a6cf094d2322e8ead31abe517598da4
noahgribbin <noahagribbin@gmail.com>
2020-12-07 11:01:09 -0800

    rm logs

--- src/controllers/withdraw.ts
+++ src/controllers/withdraw.ts
@@ -135,9 +135,7 @@ export const getWithdrawals = async (args: { status: string }, context: { user: 
 }
 
 export const getWithdrawalsV2 = async (args: { status: string }, context: { user: any }) => {
-  console.log('in get withdrawals')
   checkPermissions({ hasRole: ['admin'] }, context);
-  console.log('past permission check')
   const transfers = await Transfer.getWithdrawalsByStatus(args.status);
   const uniqueUsers: {[key: string]: any} = {};
   for (let i = 0; i < transfers.length; i++) {
