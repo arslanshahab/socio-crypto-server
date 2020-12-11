98e36e7a892ce0e667e0f1765a92b300f4b59a8c
DavidR <drigby25@protonmail.com>
2020-12-11 11:02:21 -0800

    migrations fix

--- src/migrations/1607642320956-AdminName.ts
+++ src/migrations/1607642320956-AdminName.ts
@@ -4,7 +4,7 @@ export class AdminName1607642320956 implements MigrationInterface {
     name = 'AdminName1607642320956'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "admin" ADD "name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "admin" ADD "name" character varying NOT NULL DEFAULT 'raiinmaker'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
