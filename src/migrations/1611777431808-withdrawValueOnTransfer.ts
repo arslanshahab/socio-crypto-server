e9dfb088f15437ea60953185637ac8da1fe868d1
Alex Benedetto <alex061994@gmail.com>
2021-02-18 15:21:18 -0800

    fixed migration

--- src/migrations/1611777431808-withdrawValueOnTransfer.ts
+++ src/migrations/1611777431808-withdrawValueOnTransfer.ts
@@ -6,7 +6,10 @@ export class withdrawValueOnTransfer1611777431808 implements MigrationInterface 
         await queryRunner.query(`UPDATE "transfer" SET "status" = 'PENDING' WHERE "status" = 'pending'`);
         await queryRunner.query(`UPDATE "transfer" SET "status" = 'APPROVED' WHERE "status" = 'approved'`);
         await queryRunner.query(`UPDATE "transfer" SET "status" = 'REJECTED' WHERE "status" = 'rejected'`);
-        await queryRunner.query(`UPDATE "transfer" t SET "usdAmount" = (SELECT CAST(o.amount as decimal) * 0.1 FROM public.transfer o WHERE t.id=o.id) WHERE action = 'withdraw' AND "status" IN ('PENDING','APPROVED')`);
+        await queryRunner.query(`UPDATE "transfer" SET "currency" = 'usd' WHERE "paypalAddress" != ''`);
+        await queryRunner.query(`UPDATE "transfer" SET "currency" = 'coiin' WHERE "ethAddress" != ''`);
+        await queryRunner.query(`UPDATE "transfer" t SET "usdAmount" = (SELECT CAST(o.amount as decimal) * 0.1 FROM public.transfer o WHERE t.id=o.id) WHERE action = 'withdraw' AND "status" IN ('PENDING','APPROVED') AND "currency" = 'coiin'`);
+        await queryRunner.query(`UPDATE "transfer" t SET "usdAmount" = (SELECT o.amount FROM public.transfer o WHERE t.id=o.id) WHERE action = 'withdraw' AND "status" IN ('PENDING','APPROVED') AND "currency" = 'usd'`);
         await queryRunner.query(`INSERT INTO "wallet_currency" ("type", "balance", "walletId") SELECT 'coiin' AS "type", "wallet"."balance" AS "balance", "wallet"."id" AS "walletId" FROM "wallet"`);
         await queryRunner.query(`ALTER TABLE "wallet" DROP COLUMN "balance"`);
     }
