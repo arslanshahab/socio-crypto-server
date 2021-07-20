02a1cb72957eca9c6a679b045a646919acdd79c4
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2021-07-21 01:57:46 +0500

    redis enable for exchange rate

--- src/util/forex.ts
+++ src/util/forex.ts
@@ -9,9 +9,9 @@ export const getExchangeRate = async (symbol: string) => {
         cachedResponse = JSON.parse(cachedResponse);
         return cachedResponse?.conversion_rates[symbol] || 1;
     }
-    const key = "f095f73226e7facf74af9216";
-    const exchangeRates = await doFetch(`https://v6.exchangerate-api.com/v6/${key}/latest/USD`, null, "GET", {});
-    let data = await exchangeRates.json();
+    const apiKey = "f095f73226e7facf74af9216";
+    const exchangeRates = await doFetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`, null, "GET", {});
+    const data = await exchangeRates.json();
     await getRedis().set(cacheKey, JSON.stringify(data));
     await getRedis().expire(cacheKey, 3600);
     return data?.conversion_rates[symbol] || 1;
