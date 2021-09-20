c3912802bd1ff5b4018345ed4e49297bc3c39ee6
DRigby26 <DRigby26@users.noreply.github.com>
2021-09-20 13:50:07 -0700

    added missing migrations

--- src/migrations/1626343245615-CampaignSharedMedia.ts
+++ src/migrations/1626343245615-CampaignSharedMedia.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CampaignSharedMedia1626343245615 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('ALTER TABLE "campaign" RENAME COLUMN "sharedImagePath" TO "sharedMedia"');
         await queryRunner.query('ALTER TABLE "campaign" ADD COLUMN "sharedMediaFormat" varchar');
     }
 
