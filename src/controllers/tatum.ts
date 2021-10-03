5876b02b0b55e25b55bce856f7e590ca6f75a7d6
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2021-10-03 17:52:40 +0500

    new change

--- src/controllers/tatum.ts
+++ src/controllers/tatum.ts
@@ -5,11 +5,6 @@ import { Admin } from "../models/Admin";
 // import { Request, Response } from "express";
 import { DepositAddress } from "../models/DepositAddress";
 
-const ethAddress =
-    process.env.NODE_ENV === "production"
-        ? "0x9f6fE7cF8CCC66477c9f7049F22FbbE35234274D"
-        : "0x275EE6238D103fDBE49d4cF6358575aA914F8654";
-
 // export const initWallet = asyncHandler(async (req: Request, res: Response) => {
 //     try {
 //         const { currency } = req.body;
@@ -27,34 +22,33 @@ export const getDepositAddress = async (parent: any, args: { currency: string },
         const admin = await Admin.findOne({ where: { firebaseId: id }, relations: ["depositAddress"] });
         if (!admin) throw new Error("Admi not found!");
         const fromTatum = TatumClient.isCurrencySupported(currency.toUpperCase());
+        let address: string | undefined = "";
         if (fromTatum) {
             let depositAddress = admin.depositAddress.find(
                 (item) => item.currency.toLowerCase() === currency.toLowerCase()
             );
             if (!depositAddress) {
-                let tatumAccount = await findOrCreateLedgeAccount(currency);
+                let tatumAccount = await findOrCreateLedgerAccount(currency);
                 const newDepositAddress = await TatumClient.createNewDepositAddress(tatumAccount.accountId);
                 depositAddress = await DepositAddress.addNewAddress(newDepositAddress, admin);
             }
-            return {
-                currency: currency.toUpperCase(),
-                address: depositAddress.address,
-                fromTatum,
-            };
+            address = depositAddress.address;
         } else {
-            return {
-                currency: currency.toUpperCase(),
-                address: ethAddress,
-                fromTatum,
-            };
+            address = process.env.ETHEREUM_DEPOSIT_ADDRESS;
         }
+        console.log(address);
+        return {
+            currency: currency.toUpperCase(),
+            address: address,
+            fromTatum,
+        };
     } catch (error) {
         console.log(error);
         return error;
     }
 };
 
-const findOrCreateLedgeAccount = async (currency: string): Promise<TatumAccount> => {
+const findOrCreateLedgerAccount = async (currency: string): Promise<TatumAccount> => {
     try {
         let tatumAccount = await TatumAccount.findOne({ where: { currency: currency.toUpperCase() } });
         if (!tatumAccount?.accountId) {
