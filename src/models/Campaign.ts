798dc6f9e7ad436cece3a2a99328abfc305301cd
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2021-11-12 17:39:21 +0500

    fixed errors for creating a campaign on staging

--- src/models/Campaign.ts
+++ src/models/Campaign.ts
@@ -98,6 +98,9 @@ export class Campaign extends BaseEntity {
     @Column({ nullable: true })
     public imagePath: string;
 
+    @Column({ nullable: false })
+    public currency: string;
+
     @Column({ type: "jsonb", nullable: true })
     public requirements: CampaignRequirementSpecs;
 
@@ -418,6 +421,7 @@ export class Campaign extends BaseEntity {
         keywords: string[],
         type: string,
         imagePath: string,
+        currency: string,
         campaignType: string,
         socialMediaType: string[],
         targetVideo?: string,
@@ -439,6 +443,7 @@ export class Campaign extends BaseEntity {
         campaign.imagePath = imagePath;
         campaign.campaignType = campaignType;
         campaign.socialMediaType = socialMediaType;
+        campaign.currency = currency;
         if (targetVideo) campaign.targetVideo = targetVideo;
         if (description) campaign.description = description;
         if (instructions) campaign.instructions = instructions;
