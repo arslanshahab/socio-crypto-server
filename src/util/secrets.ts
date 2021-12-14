3fe405b6e2908a1e68c81ad3f54cebbaf5c15233
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2021-12-14 19:55:41 +0500

    new changes

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -92,9 +92,9 @@ export class Secrets {
         Secrets.xoxodayClientSecret =
             process.env.XOXODAY_CLIENT_SECRET || (await readFilePromise("/var/secrets/xoxoday/clientSecret", "utf8"));
         Secrets.tatumApiKey = process.env.TATUM_API_KEY || (await readFilePromise("/var/secrets/tatum/apiKey", "utf8"));
-        Secrets.minterPrivateKey =
-            process.env.WHITELISTED_MINTER_PRIVATE_KEY ||
-            (await readFilePromise("/var/secrets/coiin-minter/privateKey", "utf8"));
+        // Secrets.minterPrivateKey =
+        //     process.env.WHITELISTED_MINTER_PRIVATE_KEY ||
+        //     (await readFilePromise("/var/secrets/coiin-minter/privateKey", "utf8"));
         Secrets.acuantApiUser =
             process.env.ACUANT_API_USER || (await readFilePromise("/var/secrets/acuant/apiUser", "utf8"));
         Secrets.acuantApiKey =
