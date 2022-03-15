a17b6a9b057b783cd1433fc3570ad3ac7643ddce
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-03-15 17:18:08 +0500

    api version bumped

--- src/migrations/1647343735486-SupportedTokens.ts
+++ src/migrations/1647343735486-SupportedTokens.ts
@@ -7,7 +7,6 @@ export class SupportedTokens1647343735486 implements MigrationInterface {
         await queryRunner.query(
             `CREATE TABLE "token" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "symbol" character varying NOT NULL, "network" character varying NOT NULL, "contractAddress" character varying, "enabled" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_82fae97f905930df5d62a702fc9" PRIMARY KEY ("id"))`
         );
-        await queryRunner.query(`ALTER TABLE "tatum_wallet" DROP COLUMN "enabled"`);
         await queryRunner.query(`ALTER TABLE "currency" ADD "tokenId" uuid`);
         await queryRunner.query(`ALTER TABLE "campaign" ADD "currencyId" uuid`);
         await queryRunner.query(
@@ -23,7 +22,6 @@ export class SupportedTokens1647343735486 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "currency" DROP CONSTRAINT "FK_d1e8baad49c6c5514b6a0dcba26"`);
         await queryRunner.query(`ALTER TABLE "campaign" DROP COLUMN "currencyId"`);
         await queryRunner.query(`ALTER TABLE "currency" DROP COLUMN "tokenId"`);
-        await queryRunner.query(`ALTER TABLE "tatum_wallet" ADD "enabled" boolean NOT NULL`);
         await queryRunner.query(`DROP TABLE "token"`);
     }
 }
