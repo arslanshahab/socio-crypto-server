aeb7362e2eeaf35bd71871cf04e12f23af319c50
Arslan Shahab <arslanshahab@gmail.com>
2022-04-14 21:25:47 +0500

    created raffle prize service

---
+++ src/services/RafflePrizeService.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from "@tsed/di";
+import { PrismaService } from ".prisma/client/entities";
+import { Campaign, RafflePrize } from "@prisma/client";
+
+@Injectable()
+export class RafflePrizeService {
+    @Inject()
+    private prismaService: PrismaService;
+
+    /**
+     * Retrieves a user object from a JWTPayload
+     *
+     * @param data the jwt payload
+     * @param include additional relations to include with the user query
+     * @returns the user object, with the requested relations included
+     */
+    public async createRafflePrize(campaign: Campaign, prize: RafflePrize) {
+        const response = await this.prismaService.rafflePrize.create({
+            data: {
+                campaignId: campaign.id,
+                displayName: prize.displayName,
+                affiliateLink: prize.affiliateLink,
+                image: prize.image,
+            },
+        });
+        return response;
+    }
+}
