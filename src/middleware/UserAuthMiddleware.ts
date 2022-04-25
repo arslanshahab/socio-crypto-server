3b002bb0ef9fb187ac7fe18ef2ec20f7c766cd2f
Arslan Shahab <arslanshahab@gmail.com>
2022-04-25 23:01:39 +0500

    working on adminAuth middleware

--- src/middleware/UserAuthMiddleware.ts
+++ src/middleware/UserAuthMiddleware.ts
@@ -1,5 +1,6 @@
-import { Context, Middleware } from "@tsed/common";
+import { Context, Middleware, Req } from "@tsed/common";
 import { Forbidden, InternalServerError } from "@tsed/exceptions";
+// import { Firebase } from "../clients/firebase";
 import { verifySessionToken } from "../util";
 
 /**
@@ -7,14 +8,27 @@ import { verifySessionToken } from "../util";
  */
 @Middleware()
 export class UserAuthMiddleware {
-    public async use(@Context() ctx: Context) {
+    public async use(@Req() req: Req, @Context() ctx: Context) {
+        // const tokenAdmin =ctx.request.req.headers.cookie;
+        // const decodedToken = await Firebase.verifySessionCookie(tokenAdmin);
+        // const firebaseUser = await Firebase.getUserById(decodedToken.uid);
+        // const admin = {
+        //     id: decodedToken.uid,
+        //     method: "firebase",
+        //     ...decodedToken,
+        //     ...(firebaseUser.customClaims && {
+        //         role: firebaseUser.customClaims.role,
+        //         company: firebaseUser.customClaims.company,
+        //         tempPass: firebaseUser.customClaims.tempPass || false,
+        //     }),
+        // };
+        // console.log("decodedToken---------------", admin);
         if (ctx.has("user") || ctx.request.url.startsWith("/v1/docs/")) {
             return;
         }
 
         const token = ctx.request.req.headers.authorization;
         if (!token) throw new Forbidden("Access token is missing.");
-
         try {
             const user = verifySessionToken(token);
             ctx.set("user", user);
