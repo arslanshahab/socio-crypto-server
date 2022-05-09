7b559e061c36b3208376cd758d55e76a4deda441
Arslan Shahab <arslanshahab@gmail.com>
2022-05-09 19:44:36 +0500

    removed extra comments from OrganizationService

--- src/services/OrganizationService.ts
+++ src/services/OrganizationService.ts
@@ -6,13 +6,6 @@ export class OrganizationService {
     @Inject()
     private prismaService: PrismaService;
 
-    /**
-     * Retrieves a user object from a JWTPayload
-     *
-     * @param data the jwt payload
-     * @param include additional relations to include with the user query
-     * @returns the user object, with the requested relations included
-     */
     public async findOrganizationByCompanyName(companyName: string) {
         return this.prismaService.org.findFirst({
             where: {
