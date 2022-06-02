a1e11e871f8e729d5303effa33c518d583d498a5
Arslan Shahab <arslanshahab@gmail.com>
2022-06-02 20:10:23 +0500

    created getWithdrawalsV2 endpoint

---
+++ src/controllers/v1/WithdrawController.ts
@@ -0,0 +1,48 @@
+import { Context, QueryParams } from "@tsed/common";
+import { Controller, Inject } from "@tsed/di";
+import { Get, Returns } from "@tsed/schema";
+import { SuccessResult } from "../../util/entities";
+import { TransferService } from "../../services/TransferService";
+import { UserService } from "../../services/UserService";
+import { TransferStatus } from "../../types";
+import { NotFound } from "@tsed/exceptions";
+import { TRANSFER_NOT_FOUND } from "../../util/errors";
+
+@Controller("/withdraw")
+export class WithdrawController {
+    @Inject()
+    private transferService: TransferService;
+    @Inject()
+    private userService: UserService;
+
+    @Get()
+    @(Returns(200, SuccessResult).Of(Object))
+    public async getWithdrawalsV2(@QueryParams() query: { status?: TransferStatus }, @Context() context: Context) {
+        this.userService.checkPermissions({ hasRole: ["admin"] }, context.get("user"));
+        const { status = "PENDING" } = query;
+        const transfers = await this.transferService.getWithdrawalsByStatus(status);
+        if (!transfers) throw new NotFound(TRANSFER_NOT_FOUND);
+        const uniqueUsers: { [key: string]: any } = {};
+        for (let i = 0; i < transfers.length; i++) {
+            const transfer = transfers[i];
+            const userId = transfer.wallet?.userId;
+            if (!uniqueUsers[userId!]) {
+                const totalWithdrawnThisYear = await this.transferService.getTotalAnnualWithdrawalByWallet(
+                    transfer.walletId!
+                );
+                const totalPendingWithdrawal = await this.transferService.getTotalPendingByCurrencyInUsd(
+                    transfer.walletId!
+                );
+                uniqueUsers[userId!] = {
+                    user: { ...transfer.wallet?.user, username: transfer.wallet?.user?.profile?.username },
+                    totalPendingWithdrawal: totalPendingWithdrawal,
+                    totalAnnualWithdrawn: totalWithdrawnThisYear,
+                    transfers: [transfer],
+                };
+            } else {
+                uniqueUsers[userId!].transfers.push(transfer);
+            }
+        }
+        return new SuccessResult(Object.values(uniqueUsers), Object);
+    }
+}
