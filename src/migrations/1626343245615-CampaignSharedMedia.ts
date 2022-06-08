5aa4a37683148aea1c4d4f324f5ee74ceaf9ca1b
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-06-09 00:06:57 +0500

    new changes

--- src/migrations/1626343245615-CampaignSharedMedia.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class CampaignSharedMedia1626343245615 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('ALTER TABLE "campaign" ADD COLUMN "sharedMediaFormat" varchar');
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
