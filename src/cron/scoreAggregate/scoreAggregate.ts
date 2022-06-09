404fba762ac599b4dbdcce7114817646b5bf2aaa
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-06-09 13:31:10 +0500

    score aggregate job refactored

--- src/cron/scoreAggregate/scoreAggregate.ts
+++ src/cron/scoreAggregate/scoreAggregate.ts
@@ -1,9 +1,9 @@
-import { getConnection } from "typeorm";
 import { Firebase } from "../../clients/firebase";
 import { BN } from "../../util";
 import * as campaignController from "../../controllers/campaign";
-import { getTokenPriceInUsd } from "../../clients/ethereum";
 import { prisma } from "../../clients/prisma";
+import { CampaignStatus } from "../../util/constants";
+import { getTokenValueInUSD } from "../../util/exchangeRate";
 
 /**
  * {
@@ -32,62 +32,78 @@ export const main = async () => {
     const paginatedUsersLoop = Math.ceil(totalUsers / take);
 
     for (let pageIndex = 0; pageIndex < paginatedUsersLoop; pageIndex++) {
-        const users = await prisma.user.findMany({ skip, take });
+        const users = await prisma.user.findMany({ include: { profile: true }, skip, take });
         for (const user of users) {
             const totalParticipationScore = await getUserTotalParticipationScore(user.id);
-            metricsToSave.push({ score: totalParticipationScore, userId: user.id });
+            metricsToSave.push({ score: totalParticipationScore.toString(), userId: user.id });
             const notificationSettings = await prisma.notificationSettings.findFirst({ where: { userId: user.id } });
+            const participations = await prisma.participant.findMany({ where: { userId: user.id } });
             try {
-                if (user.notificationSettings.campaignUpdates && user.campaigns) {
-                    const openCampaigns = user.campaigns.filter(
-                        (participant: Participant) => participant.campaign && participant.campaign.isOpen()
-                    );
-                    for (const participant of openCampaigns) {
-                        if (!campaignLeaderboards[participant.campaign.id]) {
-                            campaignLeaderboards[participant.campaign.id] = {};
-                            const sortedParticipants = participant.campaign.participants.sort(
-                                (a: Participant, b: Participant) =>
-                                    parseFloat(b.participationScore.minus(a.participationScore).toString())
-                            );
+                if (notificationSettings?.campaignUpdates && participations?.length) {
+                    const campaignIds = participations.map((item) => item.campaignId);
+
+                    const openCampaigns = await prisma.campaign.findMany({
+                        where: {
+                            id: { in: campaignIds },
+                            endDate: { gte: new Date() },
+                            status: CampaignStatus.APPROVED,
+                        },
+                    });
+                    for (const campaign of openCampaigns) {
+                        const participant = participations.find((item) => item.campaignId === campaign.id);
+                        const totalParticipants = await prisma.participant.count({
+                            where: { campaignId: campaign.id },
+                        });
+                        if (!campaignLeaderboards[campaign.id]) {
+                            campaignLeaderboards[campaign.id] = {};
+                            const sortedParticipants = await prisma.participant.findMany({
+                                where: { campaignId: campaign.id },
+                                orderBy: { participationScore: "desc" },
+                            });
                             sortedParticipants.forEach((part, idx) => {
-                                campaignLeaderboards[participant.campaign.id][part.id] = idx + 1;
+                                campaignLeaderboards[campaign.id][part.id] = idx + 1;
                             });
                         }
-                        if (!campaignInformation[participant.campaign.id])
-                            campaignInformation[participant.campaign.id] =
-                                await campaignController.getCurrentCampaignTier(null, {
-                                    campaignId: participant.campaign.id,
-                                });
-                        const latestParticipation = await DailyParticipantMetric.getLatestByParticipantId(
-                            participant.id
-                        );
-                        if (latestParticipation && latestParticipation.participationScore.gt(0)) {
+                        if (!campaignInformation[campaign.id])
+                            campaignInformation[campaign.id] = await campaignController.getCurrentCampaignTier(null, {
+                                campaignId: campaign.id,
+                            });
+                        const latestParticipation = await prisma.dailyParticipantMetric.findFirst({
+                            where: { participantId: participant?.id },
+                        });
+                        if (latestParticipation && new BN(latestParticipation.participationScore).gt(0)) {
                             const percentageOfParticipation = new BN(latestParticipation.participationScore).div(
-                                participant.campaign.totalParticipationScore
+                                campaign.totalParticipationScore
                             );
-                            const currentTotal = campaignInformation[participant.campaign.id].currentTotal;
+                            const currentTotal = campaignInformation[campaign.id].currentTotal;
 
-                            if (participant.campaign.type == "crypto") {
-                                const tokenPrice =
-                                    participant.campaign.crypto.type === "coiin"
-                                        ? 0.1
-                                        : await getTokenPriceInUsd(participant.campaign.crypto.type);
+                            if (campaign.type == "crypto") {
+                                const campaignCurrency = await prisma.currency.findFirst({
+                                    where: { id: campaign.currencyId || "" },
+                                });
+                                const campaignToken = await prisma.token.findFirst({
+                                    where: { id: campaignCurrency?.tokenId || "" },
+                                });
+                                const tokenPrice = await getTokenValueInUSD(
+                                    campaignToken?.symbol || "",
+                                    parseFloat(campaign.coiinTotal)
+                                );
                                 return await Firebase.sendDailyParticipationUpdate(
-                                    user.profile.deviceToken,
-                                    participant.campaign,
+                                    user?.profile?.deviceToken || "",
+                                    campaign.name,
                                     percentageOfParticipation.times(currentTotal).times(tokenPrice).times(10), // value in coiin
-                                    latestParticipation.participationScore,
-                                    campaignLeaderboards[participant.campaign.id][participant.id],
-                                    participant.campaign.participants.length
+                                    new BN(latestParticipation.participationScore),
+                                    campaignLeaderboards[campaign.id][participant?.id || ""],
+                                    totalParticipants
                                 );
                             } else {
                                 await Firebase.sendDailyParticipationUpdate(
-                                    user.profile.deviceToken,
-                                    participant.campaign,
+                                    user?.profile?.deviceToken || "",
+                                    campaign.name,
                                     percentageOfParticipation.times(currentTotal),
-                                    latestParticipation.participationScore,
-                                    campaignLeaderboards[participant.campaign.id][participant.id],
-                                    participant.campaign.participants.length
+                                    new BN(latestParticipation.participationScore),
+                                    campaignLeaderboards[campaign.id][participant?.id || ""],
+                                    totalParticipants
                                 );
                             }
                         }
@@ -101,6 +117,5 @@ export const main = async () => {
         skip += take;
     }
 
-    await getConnection().createEntityManager().save(metricsToSave);
-    return;
+    return await prisma.twentyFourHourMetric.createMany({ data: metricsToSave });
 };
