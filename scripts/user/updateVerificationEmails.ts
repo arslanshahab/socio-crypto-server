5a916997ffc2e06029ea6ea62e53db57f8809cb5
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-06-24 15:51:29 +0500

    new scripts

--- scripts/user/updateVerificationEmails.ts
+++ scripts/user/updateVerificationEmails.ts
@@ -10,32 +10,32 @@ dotenv.config();
         console.log("Preparing to convert username to lowercase...");
         await Secrets.initialize();
         const connection: Connection = await connectDatabase();
-        const totalUsers = await prisma.user.count();
-        const take = 50;
+        const totalApps = await prisma.verification.count();
+        const take = 100;
         let skip = 0;
-        const paginatedLoop = Math.ceil(totalUsers / take);
+        const paginatedLoop = Math.ceil(totalApps / take);
         for (let pageIndex = 0; pageIndex < paginatedLoop; pageIndex++) {
             const prismaTransactions = [];
-            const users = await prisma.user.findMany({
-                include: { profile: true },
+            const apps = await prisma.verification.findMany({
                 skip,
                 take,
                 orderBy: { createdAt: "desc" },
             });
-            for (let userIndex = 0; userIndex < users.length; userIndex++) {
-                const profile = users[userIndex].profile;
-                if (profile?.username) {
-                    console.log(profile.username);
+            for (let appIndex = 0; appIndex < apps.length; appIndex++) {
+                const app = apps[appIndex];
+                if (app.email) {
+                    console.log(app.email);
                     prismaTransactions.push(
-                        prisma.profile.update({
-                            where: { id: profile?.id || "" },
-                            data: { username: profile?.username.toLowerCase() },
+                        prisma.verification.update({
+                            where: { id: app?.id || "" },
+                            data: { email: app.email.toLowerCase() },
                         })
                     );
                 }
             }
             await prisma.$transaction(prismaTransactions);
             skip += take;
+            console.log("-----------------------------");
         }
         await connection.close();
         process.exit(0);
