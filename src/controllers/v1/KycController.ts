4080c7a86e200a323c8edf76b803c60671f949cf
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-06-28 16:16:48 +0500

    new changes

--- src/controllers/v1/KycController.ts
+++ src/controllers/v1/KycController.ts
@@ -2,13 +2,10 @@ import { BodyParams, Context, PathParams, QueryParams } from "@tsed/common";
 import { Controller, Inject } from "@tsed/di";
 import { BadRequest, NotFound } from "@tsed/exceptions";
 import { Get, Nullable, Post, Put, Required, Returns } from "@tsed/schema";
-import { KycLevel, KycStatus, RAIINMAKER_ORG_NAME } from "../../util/constants";
+import { KycLevel, RAIINMAKER_ORG_NAME } from "../../util/constants";
 import { UserService } from "../../services/UserService";
 import { SuccessResult } from "../../util/entities";
 import { KYC_LEVEL_1_NOT_APPROVED, KYC_NOT_FOUND, USER_NOT_FOUND } from "../../util/errors";
-import { getApplicationStatus, getKycStatusDetails } from "../../util";
-import { Validator } from "../../schemas";
-import { AcuantClient } from "../../clients/acuant";
 import { Firebase } from "../../clients/firebase";
 import { VerificationApplicationService } from "../../services/VerificationApplicationService";
 import { KycUser } from "../../types";
@@ -25,25 +22,6 @@ const userResultRelations = {
 class KycStatusParms {
     @Required() public readonly userId: string;
     @Required() public readonly status: string;
-}
-
-class VerifyKycParams {
-    @Required() public readonly firstName: string;
-    @Required() public readonly middleName: string;
-    @Required() public readonly lastName: string;
-    @Required() public readonly email: string;
-    @Required() public readonly billingStreetAddress: string;
-    @Required() public readonly billingCity: string;
-    @Required() public readonly billingCountry: string;
-    @Required() public readonly zipCode: string;
-    @Required() public readonly gender: string;
-    @Required() public readonly dob: string;
-    @Required() public readonly phoneNumber: string;
-    @Required() public readonly documentType: string;
-    @Required() public readonly documentCountry: string;
-    @Required() public readonly frontDocumentImage: string;
-    @Required() public readonly faceImage: string;
-    @Required() public readonly backDocumentImage: string;
 }
 
 class KycLevel1Params {
@@ -80,8 +58,6 @@ class KycLevel2Params {
     @Required() public readonly faceImage: string;
     @Required() public readonly backDocumentImage: string;
 }
-
-const validator = new Validator();
 
 @Controller("/kyc")
 export class KycController {
@@ -124,40 +100,6 @@ export class KycController {
         const user = await this.userService.findUserById(userId);
         if (!user) throw new NotFound(USER_NOT_FOUND);
         return new SuccessResult(await this.verificationApplicationService.getRawApplication(user.id), Object);
-    }
-
-    @Post("/verify-kyc")
-    @(Returns(200, SuccessResult).Of(KycResultModel))
-    public async verifyKyc(@BodyParams() query: VerifyKycParams, @Context() context: Context) {
-        const user = await this.userService.findUserByContext(context.get("user"), ["profile"]);
-        if (!user) throw new BadRequest(USER_NOT_FOUND);
-        const currentKycApplication = await this.verificationApplicationService.findKycApplication(
-            user.id,
-            KycLevel.LEVEL1
-        );
-        let verificationApplication;
-        if (!currentKycApplication || currentKycApplication.kyc.status === KycStatus.REJECTED) {
-            validator.validateKycRegistration(query);
-            const newAcuantApplication = await AcuantClient.submitApplication(query);
-            const status = getApplicationStatus(newAcuantApplication);
-            verificationApplication = await this.verificationApplicationService.upsert({
-                level: KycLevel.LEVEL2,
-                appId: newAcuantApplication.mtid,
-                status,
-                userId: user.id,
-                reason: getKycStatusDetails(newAcuantApplication),
-                record: currentKycApplication?.kyc,
-            });
-
-            Firebase.sendKycVerificationUpdate(user?.profile?.deviceToken || "", status);
-        } else {
-            verificationApplication = currentKycApplication.kyc;
-        }
-        const result = {
-            kycId: verificationApplication?.applicationId,
-            status: verificationApplication?.status,
-        };
-        return new SuccessResult(result, KycResultModel);
     }
 
     @Post("/verify/level1")
