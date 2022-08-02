da9f1778ecba506dda50528ca728b5461ac26d92
Arslan Shahab <arslanshahab@gmail.com>
2022-08-02 16:51:27 +0500

    uncomment dragonchaing

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { PlatformBuilder } from "@tsed/common";
 import logger from "./util/logger";
 import { Secrets } from "./util/secrets";
 import { authenticateAdmin, authenticateUser } from "./middleware/authentication";
-// import { Dragonchain } from "./clients/dragonchain";
+import { Dragonchain } from "./clients/dragonchain";
 import { Firebase } from "./clients/firebase";
 // import * as FactorController from "./controllers/factor";
 // import * as Dragonfactor from "@myfii-dev/dragonfactor-auth";
@@ -61,7 +61,7 @@ export class Application {
         this.databaseConnection = await this.connectDatabase();
         await Secrets.initialize();
         await Firebase.initialize();
-        // await Dragonchain.initialize();
+        await Dragonchain.initialize();
         StripeAPI.initialize();
         this.app = express();
         Sentry.init({
