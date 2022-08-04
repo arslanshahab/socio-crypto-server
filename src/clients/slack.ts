a1b8632812cbae047d0e81d12c0329de95f4af58
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2022-08-04 17:59:17 +0500

    notifications limited to prod env

--- src/clients/slack.ts
+++ src/clients/slack.ts
@@ -1,12 +1,12 @@
 import { doFetch, RequestData } from "../util/fetchRequest";
 
-const { NODE_ENV = "staging", SLACK_WEBHOOK_URL = "" } = process.env;
+const { NODE_ENV = "development", SLACK_WEBHOOK_URL = "" } = process.env;
 
 export class SlackClient {
     private static baseUrl = SLACK_WEBHOOK_URL;
 
     public static sendNotification = async (data: { name: string; error: any }) => {
-        if (NODE_ENV === "development") {
+        if (NODE_ENV === "production") {
             const { error, name } = data;
             const requestData: RequestData = {
                 method: "POST",
