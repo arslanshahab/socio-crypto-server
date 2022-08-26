194290970a0b266f47b1e28858c01faab5f9b781
Arslan Shahab <arslanshahab@gmail.com>
2022-08-26 21:27:51 +0500

    fixed findVerificationById method

--- src/services/VerificationService.ts
+++ src/services/VerificationService.ts
@@ -17,7 +17,7 @@ export class VerificationService {
 
     public async findVerificationById(id: string) {
         return readPrisma.verification.findFirst({
-            where: { id: decrypt(id), verified: true },
+            where: { id, verified: true },
         });
     }
 
@@ -57,7 +57,7 @@ export class VerificationService {
     public async verifyToken(data: { verificationToken: string; email?: string }) {
         const verificationId = decrypt(data.verificationToken);
         if (!verificationId) throw new Forbidden(EMAIL_NOT_VERIFIED);
-        const verification = await this.findVerificationById(data.verificationToken);
+        const verification = await this.findVerificationById(verificationId);
         if (!verification) throw new Forbidden(EMAIL_NOT_VERIFIED);
         if (data.email && data.email.toLowerCase() !== verification.email) throw new Forbidden(EMAIL_NOT_VERIFIED);
         if (this.isCodeExpired(verification.expiry!)) throw new Forbidden(INVALID_VERIFICATION_TOKEN);
