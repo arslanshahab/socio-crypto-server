591a5eb9f1e0028bf9e3feb084a9b5e12822ffd2
Arslan Shahab <arslanshahab@gmail.com>
2022-09-21 18:46:24 +0500

    write script for twitter username

---
+++ scripts/twitter/username.ts
@@ -0,0 +1,30 @@
+import { Connection } from "typeorm";
+import { prisma, readPrisma } from "../../src/clients/prisma";
+import { getSocialClient } from "../../src/controllers/helpers";
+import { SocialClientType } from "../../src/util/constants";
+import { decrypt } from "../../src/util/crypto";
+import { Secrets } from "../../src/util/secrets";
+import { connectDatabase } from "../helpers";
+
+async () => {
+    try {
+        console.log("Preparing to update username in social link table.");
+        await Secrets.initialize();
+        const connection: Connection = await connectDatabase();
+        const socialLinks = await readPrisma.socialLink.findMany({ where: { username: null } });
+        for (const socialLink of socialLinks) {
+            let username: string = "";
+            const client = getSocialClient(SocialClientType.TWITTER);
+            if (socialLink?.apiKey && socialLink?.apiSecret) {
+                username = await client.getUsernameV2({
+                    apiKey: decrypt(socialLink.apiKey),
+                    apiSecret: decrypt(socialLink.apiSecret),
+                });
+            }
+            await prisma.socialLink.update({ where: { id: socialLink.id }, data: { username } });
+        }
+        await connection.close();
+    } catch (error) {
+        console.log("ERROR:?", error);
+    }
+};
