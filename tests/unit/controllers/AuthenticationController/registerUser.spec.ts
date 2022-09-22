30e0c605d5c6e246128e2e67a6349c4be0a9ed9a
Ad450 <adjeimanue@gmail.com>
2022-09-22 01:48:26 +0000

    complete startVerification api test and cleanup

--- tests/unit/controllers/AuthenticationController/registerUser.spec.ts
+++ tests/unit/controllers/AuthenticationController/registerUser.spec.ts
@@ -1,68 +1,47 @@
-// import { PlatformTest } from "@tsed/common";
-// import SuperTest from "supertest";
-// import { RestServer } from "../../../../src/RestServer";
+import { PlatformTest } from "@tsed/common";
+import SuperTest from "supertest";
+import { RestServer } from "../../../../src/RestServer";
 
-// import { registerUserRoute } from "../../../test_helper";
-// import { expect } from "chai";
-// import * as authControllers from "../../../../src/controllers/v1/AuthenticationController";
-// import { UserAuthMiddleware } from "../../../../src/middleware/UserAuthMiddleware";
-// // const setEnv = () => {
-// //     process.env.NODE_ENV = "test";
-// // };
+import { registerUserRoute } from "../../../test_helper";
+import { expect } from "chai";
+import * as authControllers from "../../../../src/controllers/v1/AuthenticationController";
+import { UserAuthMiddleware } from "../../../../src/middleware/UserAuthMiddleware";
 
-// // const authSandbox = sinon.createSandbox();
-// describe(" Authentication Controller", () => {
-//     // const user = new User();
-//     // const authSandbox = createSandbox();
-//     //  const profile: Profile & {} = new Profile();
-//     // const username: string = "Emmanuel";
-//     // const verificationToken: string = "justSomeRandomToken";
-//     // const password: string = "myPassword";
-//     // const email: string = "me@raiinmaker.com";
-//     // const referralCode = "noReferralCode";
+describe(" Authentication Controller", () => {
+    let request: SuperTest.SuperTest<SuperTest.Test>;
 
-//     // let userService: UserService;
-//     // let profileService: ProfileService;
-//     // let verificationService: VerificationService;
-//     // let sessionService: SessionService;
-//     // let s3Client: S3Client;
-//     let request: SuperTest.SuperTest<SuperTest.Test>;
+    beforeAll(async () => {
+        console.log("test server started ...");
+        await PlatformTest.bootstrap(RestServer, {
+            mount: {
+                "/v1": [...Object.values(authControllers)],
+            },
+            acceptMimes: ["application/json"],
+        })();
+    });
 
-//     // let authMiddlewareSpy: jest.SpyInstance;
+    beforeAll(() => {
+        request = SuperTest(PlatformTest.callback());
+    });
+    beforeAll(() => {
+        const authMiddleware = PlatformTest.get<UserAuthMiddleware>(UserAuthMiddleware);
+        console.log(authMiddleware);
+        console.log("before was called after authMiddleware");
 
-//     // before(PlatformTest.bootstrap(RestServer, {}));
-//     beforeAll(async () => {
-//         console.log("test server started ...");
-//         await PlatformTest.bootstrap(RestServer, {
-//             mount: {
-//                 "/v1": [...Object.values(authControllers)],
-//             },
-//             acceptMimes: ["application/json"],
-//         })();
-//     });
+        jest.spyOn(authMiddleware, "use").mockImplementation(async (req, ctx) => {
+            console.log("inside spy method");
+            return;
+        });
+    });
+    afterAll(async () => {
+        await PlatformTest.reset();
+    });
 
-//     beforeAll(() => {
-//         request = SuperTest(PlatformTest.callback());
-//     });
-//     beforeAll(() => {
-//         const authMiddleware = PlatformTest.get<UserAuthMiddleware>(UserAuthMiddleware);
-//         console.log(authMiddleware);
-//         console.log("before was called after authMiddleware");
-
-//         jest.spyOn(authMiddleware, "use").mockImplementation(async (req, ctx) => {
-//             console.log("inside spy method");
-//             return;
-//         });
-//     });
-//     afterAll(async () => {
-//         await PlatformTest.reset();
-//     });
-
-//     it("should pass", async () => {
-//         const res = await request
-//             .post(registerUserRoute)
-//             .set("Authorization", "Basic token")
-//             .send({ username: "emmanuel" });
-//         expect(res.statusCode).equals(400);
-//     });
-// });
+    it("should throw BAD_REQUEST on missing param", async () => {
+        const res = await request
+            .post(registerUserRoute)
+            .set("Authorization", "Basic token")
+            .send({ username: "emmanuel", email: "me@raiinmaker.com" });
+        expect(res.statusCode).equals(400);
+    });
+});
--- tests/unit/controllers/AuthenticationController/startVerification.spec.ts
+++ tests/unit/controllers/AuthenticationController/startVerification.spec.ts
@@ -1,18 +1,20 @@
-import { PlatformTest, Req, Res } from "@tsed/common";
+import { PlatformTest } from "@tsed/common";
 import { startVerificationRoute } from "../../../test_helper";
 import { StartVerificationParams } from "../../../../src/controllers/v1/AuthenticationController";
-// import { User } from "../../../../src/models/User";
-// import { UserService } from "../../../../src/services/UserService";
-// import { VerificationType } from "../../../../src/util/constants";
-import chai from "chai";
-import chaiHttp from "chai-http";
+import { User } from "../../../../src/models/User";
+import { UserService } from "../../../../src/services/UserService";
+import { VerificationType } from "../../../../src/util/constants";
+
 import { RestServer } from "../../../../src/RestServer";
 import * as authControllers from "../../../../src/controllers/v1/AuthenticationController";
 import * as bodyParser from "body-parser";
-import { NextFunction } from "express";
-import SuperTest from "supertest";
 
-chai.use(chaiHttp);
+import SuperTest from "supertest";
+import { EMAIL_EXISTS, EMAIL_NOT_EXISTS } from "../../../../src/util/errors";
+import { Verification } from "../../../../src/models/Verification";
+import { VerificationService } from "../../../../src/services/VerificationService";
+import { SesClient } from "../../../../src/clients/ses";
+import { Firebase } from "../../../../src/clients/firebase";
 
 describe("start verification", () => {
     let request: any;
@@ -32,7 +34,6 @@ describe("start verification", () => {
                     hook: "$beforeRoutesInit",
                     use: bodyParser.urlencoded({ extended: true }),
                 },
-                testMiddleware,
             ],
         })();
     });
@@ -41,8 +42,11 @@ describe("start verification", () => {
         request = SuperTest(PlatformTest.callback());
     });
 
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it("should throw BadRequest on missing params", async () => {
-        // type set to undefined
         const body: StartVerificationParams = { email: "me@raiinmaker.com", type: undefined, admin: false };
 
         const res = await request
@@ -51,27 +55,159 @@ describe("start verification", () => {
             .set("Content-Type", "application/json")
             .set("Accept", "application/json")
             .send(JSON.stringify(body));
-        console.log(res.body.message);
+        expect(res.statusCode).toEqual(400);
     });
 
-    // it("should throw BadRequest EMAIL_EXIST", async () => {
-    //     // const body: StartVerificationParams = {
-    //     //     email: "me@gmail.com",
-    //     //     type: VerificationType.EMAIL,
-    //     //     admin: false,
-    //     // };
-    //     const testUser: User = new User();
-    //     const userService = PlatformTest.get<UserService>(UserService);
-    //     jest.spyOn(userService, "updatedUserEmail").mockResolvedValue(testUser);
+    it("should throw BadRequest EMAIL_EXIST", async () => {
+        const body: StartVerificationParams = {
+            email: "me@gmail.com",
+            type: VerificationType.EMAIL,
+            admin: false,
+        };
+        const testUser: User = new User();
+        const userService = PlatformTest.get<UserService>(UserService);
+        const spy = jest.spyOn(userService, "updatedUserEmail").mockResolvedValue(testUser);
 
-    //     const res = await request.post(startVerificationRoute).type("/json").set("content-type", "application/json");
-    //     console.log(res.body.message);
+        const res = await request
+            .post(startVerificationRoute)
+            .type("/json")
+            .set("content-type", "application/json")
+            .send(body);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message.toString()).toContain(EMAIL_EXISTS);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it("should throw EMAIL_NOT_EXISTS on type PASSWORD", async () => {
+        const body: StartVerificationParams = {
+            email: "me@gmail.com",
+            type: VerificationType.PASSWORD,
+            admin: false,
+        };
+        const userService = PlatformTest.get<UserService>(UserService);
+        const spy = jest.spyOn(userService, "updatedUserEmail").mockResolvedValue(null);
+
+        const res = await request
+            .post(startVerificationRoute)
+            .type("/json")
+            .set("content-type", "application/json")
+            .send(body);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message).toContain(EMAIL_NOT_EXISTS);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it("should throw EMAIL_NOT_EXISTS on type WITHDRAW", async () => {
+        const body: StartVerificationParams = {
+            email: "me@gmail.com",
+            type: VerificationType.WITHDRAW,
+            admin: false,
+        };
+        const userService = PlatformTest.get<UserService>(UserService);
+        const spy = jest.spyOn(userService, "updatedUserEmail").mockResolvedValue(null);
+
+        const res = await request
+            .post(startVerificationRoute)
+            .type("/json")
+            .set("content-type", "application/json")
+            .send(body);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message).toContain(EMAIL_NOT_EXISTS);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    // it("should throw EMAIL_EXISTS on admin found", async () => {
+    //     const body: StartVerificationParams = {
+    //         email: "me@raiinmaker.com",
+    //         type: VerificationType.EMAIL,
+    //         admin: true,
+    //     };
+    //     const firebaseSpy = jest.spyOn(Firebase, "getUserByEmail");
+
+    //     const res = await request
+    //         .post(startVerificationRoute)
+    //         .type("/json")
+    //         .set("content-type", "application/json")
+    //         .send(JSON.stringify(body));
+
+    //     console.log(res.body);
+
+    //     expect(res.statusCode).toEqual(400);
+    //     expect(firebaseSpy).toHaveBeenCalled();
     // });
+
+    it("should successfully start verification for a non-admin", async () => {
+        const verification = new Verification();
+
+        const body: StartVerificationParams = {
+            email: "me@raiinmaker.com",
+            type: VerificationType.EMAIL,
+            admin: false,
+        };
+
+        const userService = PlatformTest.get<UserService>(UserService);
+        const userServiceSpy = jest.spyOn(userService, "updatedUserEmail").mockResolvedValue(null);
+
+        const verificationService = PlatformTest.get<VerificationService>(VerificationService);
+        const generateVerificationSpy = jest
+            .spyOn(verificationService, "generateVerification")
+            .mockResolvedValue(verification);
+        const getDecryptedCodeSyp = jest.spyOn(verificationService, "getDecryptedCode").mockReturnValue("testing");
+        const sesClienSpy = jest
+            .spyOn(SesClient, "emailAddressVerificationEmail")
+            .mockImplementation(async (emailAddress, otp) => {
+                return true;
+            });
+
+        const res = await request
+            .post(startVerificationRoute)
+            .type("/json")
+            .set("content-type", "application/json")
+            .send(JSON.stringify(body));
+
+        expect(generateVerificationSpy).toHaveBeenCalled();
+        expect(getDecryptedCodeSyp).toHaveBeenCalled();
+        expect(userServiceSpy).toHaveBeenCalled();
+        expect(sesClienSpy).toHaveBeenCalled();
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual;
+    });
+
+    it("should successfully start verification for a admin", async () => {
+        const verification = new Verification();
+
+        const body: StartVerificationParams = {
+            email: "me@raiinmaker.com",
+            type: VerificationType.EMAIL,
+            admin: true,
+        };
+
+        const firebaseSpy = jest.spyOn(Firebase, "getUserByEmail");
+        const verificationService = PlatformTest.get<VerificationService>(VerificationService);
+        const generateVerificationSpy = jest
+            .spyOn(verificationService, "generateVerification")
+            .mockResolvedValue(verification);
+        const getDecryptedCodeSyp = jest.spyOn(verificationService, "getDecryptedCode").mockReturnValue("testing");
+        const sesClienSpy = jest
+            .spyOn(SesClient, "emailAddressVerificationEmail")
+            .mockImplementation(async (emailAddress, otp) => {
+                return true;
+            });
+
+        const res = await request
+            .post(startVerificationRoute)
+            .type("/json")
+            .set("content-type", "application/json")
+            .send(JSON.stringify(body));
+        expect(generateVerificationSpy).toHaveBeenCalled();
+        expect(getDecryptedCodeSyp).toHaveBeenCalled();
+
+        expect(sesClienSpy).toHaveBeenCalled();
+        expect(firebaseSpy).toHaveBeenCalled();
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual;
+    });
 });
-
-function testMiddleware(res: Res, req: Req, next: NextFunction) {
-    console.log("request", req);
-    console.log(req.rawHeaders);
-
-    next();
-}
