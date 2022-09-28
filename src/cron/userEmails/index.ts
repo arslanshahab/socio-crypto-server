1112e8e8b58a7d42337a559925f784b3c95f88c7
Arslan Shahab <arslanshahab@gmail.com>
2022-09-28 15:03:58 +0500

    created cron for upload emails

--- src/cron/userEmails/index.ts
+++ src/cron/userEmails/index.ts
@@ -2,8 +2,9 @@ import { S3Client } from "../../clients/s3";
 import { UserService } from "../../services/UserService";
 import * as dotenv from "dotenv";
 
-const userService = new UserService();
 dotenv.config();
+const userService = new UserService();
+
 (async () => {
     if (process.env.NODE_ENV === "production") await S3Client.uploadUserEmails(await userService.getLastHourEmails());
 })();
